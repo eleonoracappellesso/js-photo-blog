644f114f24028640c40ab761354cb5b3f12cd470
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-11-19 16:50:16 +0100

    created card template in index html

--- script/script.js
+++ script/script.js
@@ -11,3 +11,9 @@ Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API di JSO
 *Bonus*
 rendi la pagina responsive, in modo che su mobile e tablet le foto si dispongano man mano una sotto l’altra ed il titolo abbia una dimensione adeguata
 */
+
+const baseUrl = "https://jsonplaceholder.typicode.com/";
+const photos = "photos";
+
+const params = { "_limit": 6 };
+
